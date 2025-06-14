407a3aa341ec54c2057202c26764161f82a21f71
Mert Saruhan <mertsaruhan@hotmail.com>
2025-06-14 19:11:52 +0200

    feat: add question_generator feat: use question_generator on web search and rag with independent parameters feat: add model paths to config cross encoder and embedding model

--- src/chatbot/question_generator/question_generator_functions.py
+++ src/chatbot/question_generator/question_generator_functions.py
@@ -1,2 +1,40 @@
-def generate_questions(model, question, number_of_gen_questions):
-    pass
+from typing_extensions import Annotated, TypedDict
+
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_ollama import OllamaLLM
+
+from src import config
+
+QUESTION_GEN_SYS_PROMPT_PATH = config.QUESTION_GEN_SYS_PROMPT_PATH
+
+with open(QUESTION_GEN_SYS_PROMPT_PATH, "r") as f:
+    question_gen_sys_prompt = f.read()
+
+
+class GenQuestion(TypedDict):
+    """Joke to tell user."""
+
+    generated_questions: Annotated[list[str], "List of generated questions"]
+
+
+def generate_questions(model: OllamaLLM, question: str, number_of_gen_questions: int) -> list[str]:
+    """Uses plain Ollama model to generate similar questions to a given text. So the model would have a general context of the text.
+
+    Args:
+        model (OllamaLLM): plain Ollama model, no tools or structured output
+        question (str): the prompt of the user
+        number_of_gen_questions (int): number of generated questions
+
+    Returns:
+        list[str]: list of generated questions
+    """
+
+    structured_llm = model.with_structured_output(GenQuestion())
+    history = [
+        SystemMessage(question_gen_sys_prompt.format(number_of_gen_questions)),
+        HumanMessage(question),
+    ]
+    
+    response: GenQuestion = structured_llm.invoke(history)
+
+    return response.generated_questions
--- src/config.py
+++ src/config.py
@@ -2,3 +2,6 @@
 PARAM_PATH = "src\parameters.yaml"
 SYS_PROMPT_PATH = "src\chatbot\sys_prompt.txt"
 SEARCH_LLM_SYS_PROMPT_PATH = "src\chatbot\web_search\search_llm_sys_prompt.txt"
+QUESTION_GEN_SYS_PROMPT_PATH="src\chatbot\question_generator\question_gen_sys_prompt.txt"
+CROSS_ENCODER_PATH="src\models\cross_encoder"
+EMBEDDING_MODEL_PATH="src\models\embedding_model"
