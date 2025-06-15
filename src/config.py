e7f1bdb0f308f8d1512d38eccaf0da835d93ed15
Mert Saruhan <mertsaruhan@hotmail.com>
2025-06-15 17:10:36 +0200

    ci: add new paths

--- src/config.py
+++ src/config.py
@@ -1,7 +1,23 @@
 # Paths
-PARAM_PATH = "src\parameters.yaml"
-SYS_PROMPT_PATH = "src\chatbot\sys_prompt.txt"
-SEARCH_LLM_SYS_PROMPT_PATH = "src\chatbot\web_search\search_llm_sys_prompt.txt"
-QUESTION_GEN_SYS_PROMPT_PATH="src\chatbot\question_generator\question_gen_sys_prompt.txt"
-CROSS_ENCODER_PATH="src\models\cross_encoder"
-EMBEDDING_MODEL_PATH="src\models\embedding_model"
+
+
+PARAM_PATH = "src/parameters.yaml"
+
+## Main Chatbot
+SYS_PROMPT_PATH = "src/chatbot/sys_prompt.txt"
+
+## Web Search
+SUMMARY_LLM_SYS_PROMPT_PATH = "src\chatbot\summary_bot\summary_llm_sys_prompt.txt"
+
+## Question Gen
+QUESTION_GEN_SYS_PROMPT_PATH = (
+    "src/chatbot/question_generator/question_gen_sys_prompt.txt"
+)
+CROSS_ENCODER_PATH = "src/models/cross_encoder"
+
+## RAG
+EMBEDDING_MODEL_PATH = "src/models/embedding_model"
+RAG_DATA_TOPICS_PATH = "src/chatbot/RAG/rag_data_topics.json"
+
+## DECIDER LLM
+DECIDER_LLM_SYS_PROMPT = "src\chatbot\decider_llm\decide_llm_sys_prompt.txt"
