d3a22830846e5463b3307aea986f4abdb003cd7e
Mert Saruhan <mertsaruhan@hotmail.com>
2025-06-15 17:08:16 +0200

    refactor: store utils into one folder chatbot_tools and token_counter

--- src/chatbot/chatbot_tools.py
+++
@@ -1,6 +0,0 @@
-from langchain_core.tools import tool
-
-
-@tool
-def search_web_for_answer(question):
-    pass
---
+++ src/chatbot/utils/token_counter.py
@@ -0,0 +1,4 @@
+def count_message(
+    message: str,
+):
+    pass # TODO find tokenizer and install and count
