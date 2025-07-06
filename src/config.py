dafeb0980621b263113b318ba3279283f91a3a80
Mert Saruhan <mertsaruhan@hotmail.com>
2025-07-06 15:18:31 +0200

    ci: add logger to config docs: organize config

--- src/config.py
+++ src/config.py
@@ -1,36 +1,44 @@
-# Paths
 import yaml
+import logging
+import logging.config
+import json
 
+# Parameters
 PARAM_PATH = "src/parameters.yaml"
 with open(PARAM_PATH, "r") as f:
     PARAMETERS = yaml.safe_load(f)
 
-## Main Chatbot
+# Logger
+LOG_CONFIG_PATH = "src/custom_logger/logs_config.json"
+logger = logging.getLogger("assistant_logger")
+with open(LOG_CONFIG_PATH, "r") as f:
+    log_config = json.load(f)
+logging.config.dictConfig(config=log_config)
+
+
+# Paths
+## Prompts
 SYS_PROMPT_PATH = "src/chatbot/sys_prompt.txt"
-
-## Web Search
 SUMMARY_LLM_SYS_PROMPT_PATH = "src\chatbot\summary_bot\summary_llm_sys_prompt.txt"
-
-## Question Gen
 QUESTION_GEN_SYS_PROMPT_PATH = (
     "src/chatbot/question_generator/question_gen_sys_prompt.txt"
 )
-CROSS_ENCODER_PATH = "models/cross_encoder"
+DECIDER_LLM_SYS_PROMPT = "src\chatbot\decider_llm\decide_llm_sys_prompt.txt"
 
-## RAG
+
+## Models
+CROSS_ENCODER_PATH = "models/cross_encoder"
+INTERPRETER_MODEL_PATH = "models/interpreter_model"
 EMBEDDING_MODEL_PATH = "models/embedding_model"
-RAG_DATA_TOPICS_PATH = "src/chatbot/RAG/rag_data_topics.json"
-RAG_DB_PATH = "src/chatbot/doc_searchers/RAG/RAG_DB"
+
 
 ### RAG FILES
+RAG_DATA_TOPICS_PATH = "src/chatbot/RAG/rag_data_topics.json"
+RAG_DB_PATH = "src/chatbot/doc_searchers/RAG/RAG_DB"
 OCR_FILES_PATH = "data/img_to_text_files"
-OCR_FOLDER_LANG_PATH = "data/img_to_text_files/folder_languages.json"
+
 INTERPRETATION_FILES_PATH = "data/interpretation_imgs"
 TEXT_FILES_PATH = "data/texts"
-
-
-## DECIDER LLM
-DECIDER_LLM_SYS_PROMPT = "src\chatbot\decider_llm\decide_llm_sys_prompt.txt"
 
 # Metadatas
 CONTACT_JSON_PATH = "metadata/contact.json"
