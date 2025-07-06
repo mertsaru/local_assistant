bae5921c63bc6789f0e470afb10a8adabdcccc8d
Mert Saruhan <mertsaruhan@hotmail.com>
2025-07-06 09:28:02 +0200

    docs: change CrossEncoder to cross_encoder.py

--- src/chatbot/agents/__init__.py
+++ src/chatbot/agents/__init__.py
@@ -1,4 +1,4 @@
-from .cross_encoder import page_selector
+from .cross_encoder import cross_encoder
 from .decider_llm import decider_llm
 from .question_generator import question_generator
 from .summary_bot import summary_bot
--- src/chatbot/agents/cross_encoder/page_selector.py
+++
@@ -1,2 +0,0 @@
-def find_best_pages(page_list: list[str]) -> list[int]:
-    pass
