ce5c911aa73fe2741f8eac88865190169fb10819
Mert Saruhan <mertsaruhan@hotmail.com>
2025-07-13 13:05:44 +0200

    feat: create chatbot tools for shopping list

--- src/chatbot/chatbot_tools/shopping_tools.py
+++ src/chatbot/chatbot_tools/shopping_tools.py
@@ -1,16 +1,55 @@
+import json
+
+
 from langchain_core.tools import tool
 
-
-@tool
-def add_to_shopping_list(items: list[str]):
-    pass
+from src.config import SHOPPING_LIST_PATH
 
 
-@tool
-def delete_from_shopping_list(items: list[str]):
-    pass
+# TODO make shopping list categorized by aisle
 
 
 @tool
-def get_shopping_list():
-    pass
+def add_to_shopping_list(items: list[str]) -> None:
+    """Add items to the shopping list.
+
+    Args:
+        items (list[str]): The items to add to the shopping list.
+    """
+    with open(SHOPPING_LIST_PATH, "r", encoding="utf-8") as f:
+        shopping_list: list[str] = json.load(f)
+
+    shopping_list.extend(items)
+
+    with open(SHOPPING_LIST_PATH, "w", encoding="utf-8") as f:
+        json.dump(shopping_list, f, ensure_ascii=False, indent=4)
+
+
+@tool
+def delete_from_shopping_list(items: list[str]) -> None:
+    """Delete items from the shopping list.
+
+    Args:
+        items (list[str]): The items to delete from the shopping list.
+    """
+    with open(SHOPPING_LIST_PATH, "r", encoding="utf-8") as f:
+        shopping_list: list[str] = json.load(f)
+
+    for item in items:
+        if item in shopping_list:
+            shopping_list.remove(item)
+
+    with open(SHOPPING_LIST_PATH, "w", encoding="utf-8") as f:
+        json.dump(shopping_list, f, ensure_ascii=False, indent=4)
+
+
+@tool
+def get_shopping_list() -> list[str]:
+    """Get the current shopping list.
+
+    Returns:
+        list[str]: The current shopping list.
+    """
+    with open(SHOPPING_LIST_PATH, "r", encoding="utf-8") as f:
+        shopping_list: list[str] = json.load(f)
+    return shopping_list
