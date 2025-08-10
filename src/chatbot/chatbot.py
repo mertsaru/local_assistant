715c172e1c5a3d2324940bc98834fc5a3dc2c094
Mert Saruhan <mertsaruhan@hotmail.com>
2025-08-10 19:27:40 +0200

    feat: implement decider and rag search to the generator model

--- src/chatbot/chatbot.py
+++ src/chatbot/chatbot.py
@@ -2,19 +2,16 @@ from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_ollama import OllamaLLM
 
 from .chatbot_tools import reminder_tools
-
 from .agents.decider_llm import decider_llm
-from .doc_searchers.web_search import web_search
+from . import doc_searchers
 from src import config
 
 
 
-SYS_PROMPT_PATH = config.SYS_PROMPT_PATH
 
-with open(SYS_PROMPT_PATH, "r") as f:
+with open(config.SYS_PROMPT_PATH, "r") as f:
     sys_prompt = f.read()
 
-history_length_threshold = config.PARAMETERS["chat_history"]["history_length_threshold"]
 
 tools = { # TODO make it dynamic?
     "set_alarm": reminder_tools.set_alarm,
@@ -22,52 +19,59 @@ tools = { # TODO make it dynamic?
     "add_to_calendar": reminder_tools.add_to_calendar,
 }
 
+answer_gen_llm = OllamaLLM(
+    model=config.PARAMETERS["agents"]["answer_gen_llm"],
+).bind_tools(list(tools.values()))
 
 class Chatbot:
 
     def __init__(self):
-        self.history = [  # TODO create a sql base history and save every chat there, so the model can also check sql chat history like rag and web search
+        self.history = [
             SystemMessage(sys_prompt),
             HumanMessage(
                 "I am providing you some documents. I want you to answer my questions according to these documents: "
             ),
             AIMessage("Thank you for the documents, please ask your question"),
         ]
-        self.sum_tokens = 0  # TODO count sys_prompt, human message and ai message
 
-    async def generate_answer(self, model: OllamaLLM, question: str):
+        self.outer_text_sources = []  # this is used to store the text sources of the documents, so the model can use them in the answer generation
+
+        self.used_ids = []
+
+
+    async def generate_answer(self, question: str):
 
         new_doc_prompt = ""
         # run decider
-        decision = decider_llm.decide_data_source(model, question)
+        decision = decider_llm.decide_data_source(question)
 
         # if decided rag search rag
-        if decision.RAG is True:
-            pass  # TODO add rag
+        if decision.rag_search is True:
+            rag_docs = doc_searchers.rag_search.get_rag_docs(question=question, used_ids=self.used_ids)
+            new_doc_prompt += rag_docs
 
         # if decided web search, search web
         if decision.web_search is True:
-            web_docs = await web_search.get_search_results(question, model)
+            web_docs = await doc_searchers.web_search.get_search_results(question)
             new_doc_prompt += "\n\n" + web_docs
 
         if new_doc_prompt is not "":
-            pass  # TODO check token lenght
+            new_doc_prompt_token_count = answer_gen_llm.get_num_tokens(new_doc_prompt)
         else:
             new_doc_prompt_token_count = 0
 
         # check history length, if long delete some
-        if self.sum_tokens + new_doc_prompt_token_count > history_length_threshold:
-            pass  # TODO find a delete method, delete first the chat history in the middle, when it is not enough than delete part of docs
+        if  + new_doc_prompt_token_count > history_length_threshold:
+            pass  # TODO delete earliest history from used_ids and outer_text_sources
 
         # build history if any doc is found
         if new_doc_prompt != "":
             self.history[1] += new_doc_prompt
 
-        # add tools to the model
-        model_with_tools = model.bind_tools(list(tools.values()))
+
 
         # run model
-        ai_response = model_with_tools.invoke(self.history + [HumanMessage(question)])
+        ai_response = answer_gen_llm.invoke(self.history + [HumanMessage(question)])
 
         ## tool calls
         if len(ai_response.tool_calls) != 0:
@@ -76,7 +80,7 @@ class Chatbot:
                 selected_tool = tools[tool["name"].lower()]
                 tool_msg = selected_tool.invoke(tool)
                 tool_messages.append(tool_msg)
-            tool_response = model_with_tools.invoke(
+            tool_response = answer_gen_llm.invoke(
                 self.history + [HumanMessage(question)] + [ai_response] + tool_messages
             )
         else:
