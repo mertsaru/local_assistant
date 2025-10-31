4c385cd37654e0abede9676a4e56d2ce7ea9854c
Mert Saruhan <mertsaruhan@hotmail.com>
2025-10-31 18:29:10 +0100

    feat: add dev version to run on cmd

--- src/main.py
+++ src/main.py
@@ -1,30 +1,44 @@
 from pydantic import BaseModel
+import argparse
 
-from dotenv import load_dotenv
 from fastapi import FastAPI
-import uvicorn
 
-load_dotenv()
+from src.chatbot import chatbot
 
-import config
-from .chatbot import chatbot
+parser = argparse.ArgumentParser(
+    description="Local Assistant - An AI assistant that runs locally on your machine."
+)
+parser.add_argument(
+    "dev", "--dev", action="store_true", help="Run in development mode."
+)
+args = parser.parse_args()
 
 app = FastAPI()
+
 
 class ChatRequest(BaseModel):
     message: str
 
+
+agentic_chatbot = chatbot.Chatbot()
+
+
 @app.post("/chat")
-async def chat(request: ChatRequest): # TODO use await while using chatbot generate
-    response = await chatbot.generate_answer(request.message)
+async def chat(request: ChatRequest):
+    response = await agentic_chatbot.generate_answer(request.message)
     return response
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "src.main:app",
-        host="0.0.0.0",
-        port=11987,
-        reload=False,
-        log_level="info",
-        workers=1,
-    )
+
+if args.dev:
+    import asyncio
+
+    while True:
+        user_input = input("You: ")
+
+        if user_input.lower() == "exit":
+            break
+
+        request = ChatRequest(message=user_input)
+        response = asyncio.run(agentic_chatbot.generate_answer(request.message))
+
+        print("Bot: ", response)
